94971c0ef2cb5b51d092263a934569be27d504ed
Andreas Fladstad <andreasfladstad@Andreas-sin-MacBook-Pro.local>
2026-03-26 15:14:56 +0100

    added translation strings

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  no: "Norsk",
+};
+
+export const defaultLang = "no";
+
+export const ui = {
+  en: {
+    "nav.story": "Our story",
+    "nav.chapel": "Chapel",
+    "nav.contact": "Contact us",
+  },
+  no: {
+    "nav.story": "Vår historie",
+    "nav.chapel": "Kapellet",
+    "nav.contact": "Kontakt oss",
+  },
+} as const;
