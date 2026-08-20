acb7822f54cf8bd6e551a82056330d6c703ec29c
Andreas Fladstad <andreasfladstad@Mac.localdomain>
2026-08-20 11:29:58 +0200

    add the custom domain to seo info

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://andynormann.github.io",
+  site: "https://capellareginae.no",
   integrations: [sitemap()],
   experimental: {
     rustCompiler: true,
